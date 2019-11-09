9198511561e13aab3c61715ee45595c20ed943bf
judikael <judikael.bellance@epitech.eu>
2019-11-09 10:14:18 +0100

    Ajout de la fonction de listage des fichiers

--- step_1/my_tar.php
+++ step_1/my_tar.php
@@ -1,2 +1,42 @@
+#!/usr/bin/php 
 <?php
-//creation du script de compression de fichier
+$branche = array();			   //Structure des Dossier.
+$Nom_Archive = "output.mytar";//Nom de Sortie.
+$jsonArray = array();        //Intégration du tableau en json.
+$Fichier_Array = $argv;     //Stockage du tableau.
+$erreur = "";			   //Stockage des erreurs dans une variable spécifique.
+if (empty($Fichier_Array))
+{
+	echo "Veuillez entrez le nom des fichiers à archiver.\n";
+	exit;
+}
+else if (count($Fichier_Array) < 1)
+{
+	echo "Veuillez insérer au moins un dossier.\n";
+}
+
+function list_files_folders($Fichier_Array)
+{
+	global $branche; // Je donne l'accès a ces nouvelles valeur.
+	foreach ($Fichier_Array as $file) {
+		if(is_dir($file))
+		{
+			$branche = scan_folder("/".$file); //Ajout dans la variable $branche les path avec leur noms
+		}
+		else if (file_exists($file)) // Vérification de l'existance des fichiers.
+		{
+			array_push($branche, "./".$file); //Empile un ou plusieur élément a la fin du tableau.
+		}
+		else
+		{
+			$erreur : "Erreur : $file n'existe pas !";
+		}
+
+	}
+	return $tree; // On retourne le tableau avec les path des fichier.
+}
+
+function scan_folder($directory)
+{
+	global $branche
+}
